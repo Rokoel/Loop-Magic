0cc8704f571ace06f04153391ad7b8779a8ddc0d
Rokoel <rokoelvel@gmail.com>
2025-08-05 08:06:14 +0300

    Removed level editor as it is useless; Made loading textures prettier in code and easier to use

--- engine/textureLoader.js
+++ engine/textureLoader.js
@@ -1,19 +1,14 @@
-export const LIGHT_ROCK_TEXTURES = await Promise.all(
-  ["../assets/rock_tile.png", ]
-  .map(async src => createImageBitmap(await fetch(src).then(r => r.blob())))
-);
+async function returnTexture(textures) {
+  if (! Array.isArray(textures)) {
+    return await Promise.all([textures, ].map(async src => createImageBitmap(await fetch(src).then(r => r.blob()))));
+  }
+  return await Promise.all(textures.map(async src => createImageBitmap(await fetch(src).then(r => r.blob()))));
+}
 
-export const PLATFORM_TEXTURES = await Promise.all(
-  ["../assets/light_rock.png", ]
-  .map(async src => createImageBitmap(await fetch(src).then(r => r.blob())))
-);
+export const LIGHT_ROCK_TEXTURES = await returnTexture("../assets/rock_tile.png");
 
-export const RED_PLATFORM_TEXTURES = await Promise.all(
-  ["../assets/red_rock_tile.png", ]
-  .map(async src => createImageBitmap(await fetch(src).then(r => r.blob())))
-);
+export const PLATFORM_TEXTURES = await returnTexture("../assets/light_rock.png");
 
-export const SKY = await Promise.all(
-  ["../assets/lighter_sky.png", "../assets/darker_sky.png"]
-  .map(async src => createImageBitmap(await fetch(src).then(r => r.blob())))
-);
+export const RED_PLATFORM_TEXTURES = await returnTexture("../assets/red_rock_tile.png");
+
+export const SKY = await returnTexture(["../assets/lighter_sky.png", "../assets/darker_sky.png"]);
